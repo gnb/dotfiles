cc1aa49ff27fc896b3df5e64f6193b1649f1b31f
Greg Banks <gnb@fmeh.org>
2026-01-09 14:42:33 +1100

    Don't install pyright with Mason
    pyright is now in Homebrew and Mason looks pretty unnecessary

--- config/nvim/lua/gbanks/plugins/lsp/mason.lua
+++ config/nvim/lua/gbanks/plugins/lsp/mason.lua
@@ -36,7 +36,7 @@ return {
 --        "graphql",
 --        "emmet_ls",
 --        "prismals",
-        "pyright",
+--        "pyright",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
