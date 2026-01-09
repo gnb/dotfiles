7ec55b9504faf492f0d8ad408c1851413adb45ba
Greg Banks <gnb@fmeh.org>
2026-01-09 16:55:30 +1100

    Adjust LSP related keymaps for nvim 0.11
    - Removed keymaps which duplicate the new default keymaps
    - Updated key sequences for some keymaps which use Telescope
      to present information, to match new default keymaps which
      show the same information

--- config/nvim/lua/gbanks/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/gbanks/plugins/lsp/lspconfig.lua
@@ -19,41 +19,31 @@ return {
       opts.buffer = bufnr
 
       -- set keybinds
-      opts.desc = "Show LSP references"
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      -- nvim 0.11+ has a default keymap grr which does the same as our gR
 
-      opts.desc = "Go to declaration"
-      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+      -- replace the default gd keymap with an LSP-driven one which is
+      -- smarter about finding definitions
+      opts.desc = "Go to definition (LSP)"
+      keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- go to definition
 
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      -- nvim 0.11+ has a default keymap gri which shows the same info as our gi
+      -- it seems never to have been used so isn't Telescope-ized here
 
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
+      -- nvim 0.11+ has a default keymap grt which shows the same info,
+      -- but Telescope's builtin picker shows it better
       opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+      keymap.set("n", "grt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      opts.desc = "Smart rename"
-      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+      -- nvim 0.11+ has a default keymap gra which does the same as our <leader>ca
+      -- nvim 0.11+ has a default keymap grn which does the same as our <leader>rn
 
       opts.desc = "Show buffer diagnostics"
       keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-      opts.desc = "Show line diagnostics"
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+      -- nvim 0.11+ has a default keymap <C-w>d which does the same as our <leader>d
+      -- nvim 0.11+ defines the same [d keymap we had here
+      -- nvim 0.11+ defines the same ]d keymap we had here
+      -- nvim 0.11+ defines the same K keymap we had here
 
       opts.desc = "Restart LSP"
       keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
